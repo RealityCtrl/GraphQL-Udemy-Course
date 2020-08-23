0691f3db628c99e49a2d2ec28d78bc93019045e1
David McCandless <david.mccandless@outlook.com>
2020-08-23 19:11:35 +0100

    Section 2 video 16 arrays

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,19 +1,59 @@
 import {GraphQLServer} from 'graphql-yoga'
+const { v4: uuidv4 } = require('uuid');
 
 const typeDefs = `
     type Query {
-        hello: String!
+        greeting(name: String): String
+        me: User
+        post: Post,
+        add(a: Float!, b: Float!): Float!
+        grades: [Int!]!
+    },
+    type User {
+        id: ID!
         name: String!
+        email: String
+        age: Int
+    }
+    type Post {
+        id: ID!
+        title: String!
+        body: String,
+        published: String
     }
 `
 
 const resolvers = {
     Query: {
-        hello(){
-            return 'This is my first query!'
+        greeting(parent, args, ctx, info){
+            if(args.name){
+                //console.log(parent, args, ctx, info)
+                return `Hello ${args.name}, welcome!`
+            }else{
+                return "Hello"
+            }
         },
-        name(){
-            return 'David McCandless'
+        add(parent, args, ctx, info){
+            return args.a + args.b
+        },
+        grades(parent, args, ctx, info){
+            return [75, 54, 88, 43]
+        },
+        me(){
+            return {
+                id: uuidv4(),
+                name: 'David McCandless',
+                email: null, 
+                age: 99
+            }
+        },
+        post(){
+            return {
+                id: uuidv4(),
+                title: 'My first blog post!',
+                body: ' This is the body of a blog post',
+                published: '2020-08-22'
+            }
         }
     }
 }
