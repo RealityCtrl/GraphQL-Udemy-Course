fc162414bddc1d032b3c1bae1b3cd72380e3fe52
David McCandless <david.mccandless@outlook.com>
2020-08-30 17:52:53 +0100

    completed arrays videos

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,48 @@
 import {GraphQLServer} from 'graphql-yoga'
 const { v4: uuidv4 } = require('uuid');
 
+const userData = [
+    {
+        id: uuidv4(),
+        name: 'David McCandless',
+        email: null, 
+        age: 99
+    },
+    {
+        id: uuidv4(),
+        name: 'User2',
+        email: 'a@b.com', 
+        age: 33
+    },
+    {
+        id: uuidv4(),
+        name: 'User3',
+        email: 'a@example.com', 
+        age: 44
+    }
+]
+
+const postsData = [
+    {
+        id: uuidv4(),
+        title: 'My first blog post!',
+        body: ' This is the body of a blog post',
+        published: '2020-08-22'
+    },
+    {
+        id: uuidv4(),
+        title: 'My second blog post!',
+        body: ' This a new blog post to test querying',
+        published: '2020-08-23'
+    },
+    {
+        id: uuidv4(),
+        title: 'My Third blog post!',
+        body: ' This is the body of a blog post and its dummy data!',
+        published: '2020-08-24'
+    }
+]
+
 const typeDefs = `
     type Query {
         greeting(name: String): String
@@ -8,6 +50,9 @@ const typeDefs = `
         post: Post,
         add(a: Float!, b: Float!): Float!
         grades: [Int!]!
+        addNumbers(numbers: [Float]!): Float!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
     },
     type User {
         id: ID!
@@ -36,7 +81,16 @@ const resolvers = {
         add(parent, args, ctx, info){
             return args.a + args.b
         },
-        grades(parent, args, ctx, info){
+        addNumbers(parent, args, ctx, info){
+            let result = 0
+            if(args.numbers.lenghth !== 0){
+                result = args.numbers.reduce((accumulator, currentValue) => {
+                    return accumulator + currentValue;
+                })
+            }
+            return result
+        },
+        grades(){
             return [75, 54, 88, 43]
         },
         me(){
@@ -54,6 +108,22 @@ const resolvers = {
                 body: ' This is the body of a blog post',
                 published: '2020-08-22'
             }
+        },
+        users(parent, args, ctx, info){
+            if(args.query){
+                return userData.filter((user) => {
+                   return user.name.toLowerCase().includes(args.query.toLowerCase())
+                })
+            }
+            return userData
+        },
+        posts(parent, args, ctx, info){
+            if(args.query){
+                return postsData.filter((post) => {
+                   return post.body.toLowerCase().includes(args.query.toLowerCase()) || post.title.toLowerCase().includes(args.query.toLowerCase());
+                })
+            }
+            return postsData
         }
     }
 }
