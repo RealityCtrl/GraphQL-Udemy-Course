a03aa02ebb84e1adb23fdae907d7d7712822e4a6
David McCandless <david.mccandless@bazaarvoice.com>
2021-01-28 15:53:22 +0000

    Completed Section 4: GraphQL Basics: Subscriptions

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -1,5 +1,4 @@
 const { v4: uuidv4 } = require('uuid');
-
 const Mutation = {
     createUser(parent, args, {db}, info){
         const emailInUse = db.userData.some((user) => {
@@ -15,40 +14,54 @@ const Mutation = {
         db.userData.push(newUser);
         return newUser;
     },
-    createPost(parent, args, {db}, info){
+    createPost(parent, {data}, {db, pubSub}, info){
         const userExists = db.userData.some((user) => {
-            return user.id === args.data.author;
+            return user.id === data.author;
         })
         if(!userExists){
-            throw new Error(`User ${args.data.author} not found`);
+            throw new Error(`User ${data.author} not found`);
         }
 
         const post = {
             id: uuidv4(),
-            ...args.data
+            ...data
         };
         db.postsData.push(post);
+        if(post.published){
+            pubSub.publish(`post`, {
+                post: {
+                    mutation: 'CREATED',
+                    data: post
+                }
+            })
+        }
         return post;
     },
-    createComment(parent, args, {db}, info){
+    createComment(parent, {data}, {db, pubSub}, info){
 
         const userExists = db.userData.some((user) => {
-            return user.id === args.data.author;
+            return user.id === data.author;
         })
         if(!userExists){
-            throw new Error(`User ${args.data.author} not found`);
+            throw new Error(`User ${data.author} not found`);
         }
         const postExists = db.postsData.some((post)=>{
-            return post.id === args.data.post;
+            return post.id === data.post;
         })
         if(!postExists){
-            throw new Error(`Post ${args.data.post} not found`);
+            throw new Error(`Post ${data.post} not found`);
         }
         const comment = {
             id: uuidv4(),
-            ...args.data
+            ...data
         };
         db.commentData.push(comment);
+        pubSub.publish(`comment ${data.post}`, {
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
+        })
         return comment;
     },
     updateUser(parent, args, {db}, info){
@@ -78,92 +91,80 @@ const Mutation = {
         }
         return user;
     },
-    createPost(parent, args, {db}, info){
-        const userExists = db.userData.some((user) => {
-            return user.id === args.data.author;
-        })
-        if(!userExists){
-            throw new Error(`User ${args.data.author} not found`);
-        }
-
-        const post = {
-            id: uuidv4(),
-            ...args.data
-        };
-        db.postsData.push(post);
-        return post;
-    },
-    updatePost(parent, args, {db}, info){
+    updatePost(parent, {id, data}, {db, pubSub}, info){
         const post = db.postsData.find((post) => {
-            return post.id === args.id;
+            return post.id === id;
         })
         if(!post){
-            throw new Error(`Post ${args.id} not found`);
+            throw new Error(`Post ${id} not found`);
         }
-        if(args.data.title && typeof args.data.title === 'string'){
-            post.title = args.data.title
+        if(data.title && typeof data.title === 'string'){
+            post.title = data.title
         }
 
-        if(args.data.body && typeof args.data.body === 'string'){
-            post.body = args.data.body
+        if(data.body && typeof data.body === 'string'){
+            post.body = data.body
         }
-        if(typeof args.data.published === 'boolean'){
-            post.published = args.data.published
-        }
-        if(args.data.author && typeof args.data.author === 'string'){
+        if(data.author && typeof data.author === 'string'){
 
             const userExists = db.userData.some((user) => {
-                return user.id === args.data.author;
+                return user.id === data.author;
             })
             if(!userExists){
-                throw new Error(`User ${args.data.author} not found`);
+                throw new Error(`User ${data.author} not found`);
             }
-            post.author = args.data.author
+            post.author = data.author
+        }
+        let event = "UPDATED";
+        if(typeof data.published === 'boolean'){
+            if(data.published && !post.published){
+                event = 'CREATED';
+            }else if(!data.published && post.published){
+                event = 'DELETED';
+            }else if(!data.published && !post.published){
+                event = ''
+            }
+            post.published = data.published
+        }else if(!post.published){
+            event = ''
+        }
+        if(event){
+            pubSub.publish(`post`, {
+                post: {
+                    mutation: event,
+                    data: post
+                }
+            })
         }
 
         return post;
     },
-    createComment(parent, args, {db}, info){
-
-        const userExists = db.userData.some((user) => {
-            return user.id === args.data.author;
-        })
-        if(!userExists){
-            throw new Error(`User ${args.data.author} not found`);
-        }
-        const postExists = db.postsData.some((post)=>{
-            return post.id === args.data.post;
-        })
-        if(!postExists){
-            throw new Error(`Post ${args.data.post} not found`);
-        }
-        const comment = {
-            id: uuidv4(),
-            ...args.data
-        };
-        db.commentData.push(comment);
-        return comment;
-    },
-    updateComment(parent, args, {db}, info){
+    updateComment(parent, {id, data}, {db, pubSub}, info){
         const comment = db.commentData.find((comment) => {
-            return comment.id === args.id;
+            return comment.id === id;
         })
         if(!comment){
-            throw new Error(`Post ${args.id} not found`);
+            throw new Error(`Post ${id} not found`);
         }
-        if(args.data.text && typeof args.data.text === 'string'){
-            comment.text = args.data.text
+        if(data.text && typeof data.text === 'string'){
+            comment.text = data.text
         }
-        if(args.data.author && typeof args.data.author === 'string'){
+        if(data.author && typeof data.author === 'string'){
 
             const userExists = db.userData.some((user) => {
-                return user.id === args.data.author;
+                return user.id === data.author;
             })
             if(!userExists){
-                throw new Error(`User ${args.data.author} not found`);
+                throw new Error(`User ${data.author} not found`);
             }
-            comment.author = args.data.author
+            comment.author = data.author
         }
+        pubSub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        })
         return comment;
     },
     deleteUser(parent, args, {db}, info){
@@ -190,29 +191,42 @@ const Mutation = {
 
         return deletedUsers[0]
     },
-    deletePost(parent, args, {db}, info){
+    deletePost(parent, {postID}, {db,pubSub}, info){
         const postIndex = db.postsData.findIndex((post) => {
-            return post.id === args.postID;
+            return post.id === postID;
         });
         if(postIndex === -1){
-            throw new Error(`Post ${args.postID} not found`);
+            throw new Error(`Post ${postID} not found`);
         }
-        const deletedPosts= db.postsData.splice(postIndex,1);
+        const [deletedPost]= db.postsData.splice(postIndex,1);
         db.commentData = db.commentData.filter((comment)=>{
-            return comment.post !== args.postID;
+            return comment.post !== postID;
         })
-
-        return deletedPosts[0];
+        if(deletedPost.published){
+            pubSub.publish(`post`, {
+                post: {
+                    mutation: 'DELETED',
+                    data: deletedPost
+                }
+            })
+        }
+        return deletedPost;
     },
-    deleteComment(parent, args, {db}, info){
+    deleteComment(parent, {commentID}, {db, pubSub}, info){
         const commentIndex = db.commentData.findIndex((comment) => {
-            return comment.id === args.commentID;
+            return comment.id === commentID;
         });
         if(commentIndex === -1){
-            throw new Error(`Comment ${args.commentID} not found`);
+            throw new Error(`Comment ${commentID} not found`);
         }
-        const deletedComments= db.commentData.splice(commentIndex,1);
-        return deletedComments[0];
+        const [deletedComment]= db.commentData.splice(commentIndex,1);
+        pubSub.publish(`comment ${deletedComment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: deletedComment
+            }
+        })
+        return deletedComment;
     }
 }
 
--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,14 +1,19 @@
 const Subscription = {
-    count: {
-        subscribe(parent, args, {pubSub}, info){
-            let count = 0;
-            setInterval(() =>{
-                count++;
-                pubSub.publish('count', {
-                    count: count
-                })
-            }, 1000)
-            return pubSub.asyncIterator('count')
+    comment: {
+        subscribe(parent, {postID}, {db, pubSub}, info){
+            const post = db.postsData.find((post) =>
+                post.id === postID && post.published
+            )
+            if(!post){
+                throw new Error(`Post not found for ${postID}`)
+            }
+
+            return pubSub.asyncIterator(`comment ${postID}`)
+        }
+    },
+    post: {
+        subscribe(parent, {postID}, {pubSub}, info){
+            return pubSub.asyncIterator(`post`)
         }
     }
 }
